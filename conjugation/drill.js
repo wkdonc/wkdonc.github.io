40e98b6be3cc8057d89e57037235a1f93a64a015
Maurice Ampt <mauriceampt@hotmail.com>
2021-06-13 00:07:48 +0200

    Used localstorage to store and load options chosen by user.

--- conjugation/drill.js
+++ conjugation/drill.js
@@ -4,6 +4,14 @@ var transformations = [];
 
 var log;
 
+var options = ["plain", "polite", "negative", "past", "te-form",
+"progressive", "potential", "imperative", "passive", "causative",
+"godan", "ichidan", "iku", "kuru", "suru", "i-adjective", "na-adjective",
+"ii", "desire", "volitional", "trick", "kana", "furigana_always",
+"use_voice"];
+
+var selects = ["questionFocus"];
+
 Array.prototype.randomElement = function () {
   return this[Math.floor(Math.random() * this.length)]
 }
@@ -626,6 +634,7 @@ function showSplash() {
 function startQuiz() {
 
   var options = getOptions();
+  localStorage.setItem('options', JSON.stringify(options));
 
   const voiceSelectError = document.querySelector('#voiceSelectError');
 
@@ -970,15 +979,6 @@ function explain() {
 }
 
 function getOptions() {
-
-  var options = ["plain", "polite", "negative", "past", "te-form",
-    "progressive", "potential", "imperative", "passive", "causative",
-    "godan", "ichidan", "iku", "kuru", "suru", "i-adjective", "na-adjective",
-    "ii", "desire", "volitional", "trick", "kana", "furigana_always",
-    "use_voice"];
-
-  var selects = ["questionFocus"];
-
   var result = {};
 
   options.forEach(function (option) {
@@ -992,6 +992,18 @@ function getOptions() {
   return result;
 }
 
+function loadOptions() {
+  var storedOptions = JSON.parse(localStorage.getItem('options'));  
+
+  options.forEach(function (option) {
+    ($('#' + option)).checked = storedOptions[option];
+  });
+
+  selects.forEach(function (select) {
+    $('#' + select).value = storedOptions[select];
+  });
+}
+
 $('window').ready(function () {
 
 	if (window.speechSynthesis) {
@@ -1003,6 +1015,7 @@ $('window').ready(function () {
 
   calculateAllConjugations();
   calculateTransitions();
+  loadOptions();
 
   $('#go').click(startQuiz);
   $('#backToStart').click(showSplash);
